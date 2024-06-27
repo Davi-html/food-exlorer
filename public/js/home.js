7c0d51422f5fe27f5888783830fb63fe06ce44ac
Davi-html <davifernandes3674@gmail.com>
2024-06-27 20:05:34 -0300

    criando o carrosel das refeições, includes, header do site, menu hamburguer e etc

--- public/js/home.js
+++ public/js/home.js
@@ -2,18 +2,53 @@ function show(){
   menu = document.querySelector('.menu')
   hamburguer = document.querySelector('.hamburguer')
   x = document.querySelector('.x')
- 
-  menu.style.display = 'none'
+  cart = document.querySelector('.cart')
+  title = document.querySelector('.title')
+  textMenu = document.querySelector('.text-menu')
+
+
+  menu.style.display = 'flex'
   hamburguer.style.display = 'none'
   x.style.display = 'block'
+  title.style.display = 'none'
+  cart.style.display = 'none'
+  textMenu.style.display = 'block'
 }
 
 function fechar(){
   menu = document.querySelector('.menu')
   hamburguer = document.querySelector('.hamburguer')
   x = document.querySelector('.x')
+  cart = document.querySelector('.cart')
+  title = document.querySelector('.title')
+  textMenu = document.querySelector('.text-menu')
+  
  
   x.style.display = 'none'
-  menu.style.display = 'flex'
+  menu.style.display = 'none'
   hamburguer.style.display = 'block'
+  title.style.display = 'flex'
+  cart.style.display = 'block'
+  textMenu.style.display = 'none'
 }
+
+  const imgs = document.querySelector('#img-carrosel');
+  const img = document.querySelectorAll('#img-carrosel img');
+
+  let idx = 0;
+
+  function carroselRight() {
+    idx++;
+    if (idx > img.length - 1) {
+      idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 88}px)`;
+  }
+
+  function carroselLeft() {
+    idx--;
+    if (idx < 0) {
+      idx = img.length - 1;
+    }
+    imgs.style.transform = `translateX(${-idx * 88}px)`; // Movimento para a esquerda
+  }
